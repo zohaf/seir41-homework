202e443828988b1ead26633fa81dd9efa9954510
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-01-11 17:25:58 +1100

    update

---
+++ Zoha Alizadeh Fard/wk 4/04 thur/js/shell.js
@@ -0,0 +1,56 @@
+const $links = $('a');
+
+href = $link.attr('href');
+
+Image.attr('src', thumbnailURL)
+
+$('a').each(function () {
+thissc
+
+});
+var $mylink = $(link)
+
+$mylink.html()
+
+
+$mylink.attr('href')
+$mylink.css('font-size');//20px
+
+$('<p></p>');
+$('<p>welcome!</p>')
+
+
+$myPara = $()
+$myPara.css({color:})
+
+append propend 
+
+
+var callback = function (name) {
+
+
+}
+
+$('button').on('click', function(){
+    console.log('clicked!')
+});
+
+
+$('a').on('click', function (event){
+
+})
+
+
+const thumbnailify = function ($link) {
+    const href = $link.attr('href');
+
+    $link.prepend$img()
+}
+$link.on('click', function (event) {
+    event.preventDefault();
+    console.log('wow');
+
+});
+.html(embedHTML);
+
+const embedHTML = `<iframe >`
